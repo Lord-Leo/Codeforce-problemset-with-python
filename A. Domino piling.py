4b9a69e27c53be481d2532a5cda06611da23c199
Lord-Leo <130584018+Lord-Leo@users.noreply.github.com>
2025-09-16 23:10:03 +0600

    Add files via upload

---
+++ A. Domino piling.py
@@ -0,0 +1,7 @@
+m,n = map(int, input().split())     #this map,int,input,split have to use in python for getting a
+                                    #sequancial value. Like here, if you try to give 2 4 without
+                                   #pressing the enter button after one number(2)(without using those)
+                                    #there will be an error. but using them you can just give
+                                    # any value in one line
+num=(m*n)/2
+print(int(num))
